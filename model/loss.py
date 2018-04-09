bdd3a07d526b8d21a8aec2a04116fab44e1a30b2
lhq1208 <1972573764@qq.com>
2018-04-09 11:24:29 +0800

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -12,7 +12,7 @@ class FocalLoss(nn.Module):
         self.num_classes = num_classes
         self.FloatTensor = torch.cuda.FloatTensor if use_gpu else torch.FloatTensor
 
-    def focal_loss(self, x, y, gamma=2):
+    def focal_loss(self, x, y, gamma=2, alpha=0.25):
         """
         Focal loss.
 
@@ -24,17 +24,19 @@ class FocalLoss(nn.Module):
           (tensor) focal loss.
         """
         t = one_hot_embedding(y.data.cpu(), self.num_classes+1)
-        t = t[:, 1:]
+        t = t[1:, 1:]
         t = Variable(t).type(self.FloatTensor)  # [N,D]
         p = F.softmax(x, dim=1)  # [N,D]
         pt = (p * t).sum(1)  # [N,]
-        loss = F.cross_entropy(x, y, reduce=False)  # [N,]
-        loss = (1 - pt).pow(gamma) * loss
-
-        return loss.sum()
+        print(t, pt)
+        # loss = F.cross_entropy(x, y, reduce=False)  # [N,]
+        # loss = (1 - pt).pow(2) * loss
+        loss = -alpha*(1-pt).pow(gamma)*torch.log(pt)
+        return loss.mean()
 
     def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
-        '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
+        """
+        Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
 
         Args:
           loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
@@ -44,7 +46,7 @@ class FocalLoss(nn.Module):
 
         loss:
           (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
-        '''
+        """
         batch_size, num_boxes = cls_targets.size()
         pos = cls_targets > 0  # [N,#anchors]
         num_pos = pos.data.long().sum()
@@ -63,7 +65,7 @@ class FocalLoss(nn.Module):
         pos_neg = cls_targets > -1  # exclude ignored anchors
         mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
         masked_cls_preds = cls_preds[mask].view(-1, self.num_classes)
-        cls_loss = self.focal_loss2(masked_cls_preds, cls_targets[pos_neg])
+        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
 
         print('loc_loss: %.3f | cls_loss: %.3f' % (loc_loss.data[0]/num_pos, cls_loss.data[0]/num_pos), end=' | ')
         loss = (loc_loss+cls_loss)/num_pos
@@ -71,10 +73,13 @@ class FocalLoss(nn.Module):
 
 
 if __name__ == '__main__':
-    f = FocalLoss()
-    x = torch.rand(3, 20)
-    y = torch.zeros(3,).long()
-    y[0] = 1
+    f = FocalLoss(3)
+    x = torch.rand(3, 3)
+    y = torch.zeros(4,).long()
+    y[0] = 0
+    y[1] = 1
+    y[2] = 2
+    y[3] = 3
     x, y = Variable(x), Variable(y)
     z1 = f.focal_loss(x, y)
     print(z1)
